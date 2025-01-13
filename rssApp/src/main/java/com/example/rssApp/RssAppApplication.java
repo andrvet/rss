46ba56353df8a70208c35b36250ba6b89a2c0418
avetushko <avetushko@natera.com>
2025-01-13 15:42:53 -0800

    json logging for prod; raw logging otherwise

--- rssApp/src/main/java/com/example/rssApp/RssAppApplication.java
+++ rssApp/src/main/java/com/example/rssApp/RssAppApplication.java
@@ -1,12 +1,18 @@
 package com.example.rssApp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RssAppApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(RssAppApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Service started");
+		logger.error("An error occurred", new RuntimeException("Example exception"));
 		SpringApplication.run(RssAppApplication.class, args);
 	}
 
